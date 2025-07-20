eee1dedc83839790b27685760d072cb007d78ff7
Prabakaran Shankar <madanprabakar@gmail.com>
2025-07-20 14:16:33 +0900

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,21 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import cloudflare from '@astrojs/cloudflare';
+import path from 'path';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://astroweb.dev',
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare(),
+  trailingSlash: 'always', 
+  
+    vite: {
+      resolve: {
+        alias: {
+          '@': path.resolve('./src')
+        }
+      }
+    },
 });
