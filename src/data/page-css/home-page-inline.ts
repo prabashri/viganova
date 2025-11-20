10c7a2a1c107e9bd0b7cfe668c4aa0bd5d4df1ae
Prabakaran Shankar <madanprabakar@gmail.com>
2025-11-20 22:15:49 +0900

    about-us page update and logo text update

---
+++ src/data/page-css/home-page-inline.ts
@@ -0,0 +1,3 @@
+// AUTO-GENERATED: inline CSS for page "home-page-inline"
+const css = ".w-5-0{width:clamp(40px,25.455px + 4.545vw,80px)}.h-5-0{height:clamp(40px,25.455px + 4.545vw,80px)}";
+export default css;
